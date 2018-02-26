20ae9857e36e29bb1998dd1384ad71bf567298f5
Chap Ambrose <chapambrose@gmail.com>
2018-02-26 09:35:46 -0600

    sleep /articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,8 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    sleep(params[:sleep])
+    render {status:ok}.to_json
   end
 
   # GET /articles/1
