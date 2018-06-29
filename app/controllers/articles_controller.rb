d1bc4f30ba8e89f32f0925037698024fd8b4f00e
Chap Ambrose <chapambrose@gmail.com>
2018-06-29 08:42:06 -0500

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    render text: params
   end
 
   # GET /articles/1
